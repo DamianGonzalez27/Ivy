5aa89c5c6998f880c4ee537dde3af6a86cbfd7ae
Erick Damian <ing.gonzaleza@outlook.com>
2019-07-25 17:08:43 -0500

    Depuracion de codigo

--- vistas/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'app/config.inc.php';
-include_once 'app/controlSesion.inc.php';
-include_once 'app/redireccion.inc.php';
-ControlSesion::CerrarSesion();
-Redireccion::redirigir(SERVIDOR);
- ?>
