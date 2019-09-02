fba58a895c31841a88a310436f06ac8b996e3a09
DamianDev <ing.gonzaleza@outlook.com>
2019-09-02 18:03:31 -0500

    Inicio

--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+* @author Damian Gonzalez
+* @since 08/2019
+* En este archivo se encuentra inicializado toda la logica para comenzar con el framework
+* 
+*/
+
+
+
 $ruta = "../Front/vistas/home.html";
 
 include_once $ruta;
