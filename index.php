06371a229af612de6f944b91bee5e5ac67f2e2fa
DamianDev <ing.gonzaleza@outlook.com>
2019-09-29 17:00:40 -0500

    Primera estructura de rutas
    Se crea un metodo por medio del cual se genran rutas y respuesta
    De metodos POST en el Framework

--- index.php
+++ index.php
@@ -6,28 +6,9 @@
 * En este archivo se encuentra inicializado toda la logica para comenzar con el framework
 *
 */
-$componentes_url = parse_url($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
 
-$ruta = $componentes_url['path'];
+include_once "Framework/kernel.php";
 
-$partes_ruta = explode('/', $ruta);
-$partes_ruta = array_filter($partes_ruta);
-$partes_ruta = array_slice($partes_ruta, 0);
+$app = new init;
 
-include_once "Framework/init.php";
-
-
-if ($partes_ruta[0] == "ivyframe.test")
-{
-
-    $app = new init;
-    $app->start();
-
-
-
-}
-
-function funcionTest()
-{
-  return "test";
-}
+$app -> start();
